1db7978b9482ac656b07c05815544467e662c7a0
krrakshit <rkrkr365@gmail.com>
2025-02-19 22:57:39 +0530

    ...

--- portfolio/app/components/Iconcloud.tsx
+++ portfolio/app/components/Iconcloud.tsx
@@ -80,6 +80,7 @@ export default function IconCloud({ iconSlugs }: DynamicCloudProps) {
   }, [data, theme]);
 
   return (
+    
     <Cloud {...cloudProps}>
       <>{renderedIcons}</>
     </Cloud>
