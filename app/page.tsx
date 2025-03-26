9b555f668b24deca059141839600b737a157a99b
krrakshit <rkrkr365@gmail.com>
2025-03-26 17:56:59 +0530

    added SplashCursor component to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import TechStack from "./components/TechStack"
 import LetsConnect from "./components/LetsConnect"
+import SplashCursor from "@/components/SplashCursor"
 
 export default function Home() {
   return (
     <div className="space-y-12">
+      <SplashCursor/>
       <h1 className="text-4xl font-bold text-center">Welcome to My Portfolio</h1>
       <TechStack />
       <LetsConnect />
