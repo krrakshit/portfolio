3b1fdca96f417cdf8dab044b7aaa861c0446f229
krrakshit <rkrkr365@gmail.com>
2025-04-13 01:21:10 +0530

    refactor Tailwind CSS configuration and global styles; update GoogleGeminiEffect component with new title and email, and remove unused component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useScroll, useTransform } from "framer-motion";
 import React from "react";
-import { GoogleGeminiEffect } from "../components/ui/google-gemini-effect";
+import { GoogleGeminiEffect } from "./components/google-gemini-effect";
 import TechStack from "./components/TechStack";
 import LetsConnect from "./components/LetsConnect";
 import SplashCursor from "@/components/SplashCursor";
@@ -42,6 +42,11 @@ export default function Home() {
         <TechStack />
         <Certificates />
         
+        <div className="py-10">
+          <div
+            className="h-[400vh] w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
+            ref={ref}
+          >
             <GoogleGeminiEffect
               pathLengths={[
                 pathLengthFirst,
@@ -51,9 +56,11 @@ export default function Home() {
                 pathLengthFifth,
               ]}
             />
+          </div>
         </div>
         
         <LetsConnect />
       </div>
+    </div>
   );
 }
